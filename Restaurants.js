5915ffdac0e4ad59c334537e503297d78785672f
MouTurE <78376689+MouTurE@users.noreply.github.com>
2024-08-27 13:02:48 +0300

    Updated 'Restaurants' page

---
+++ Restaurants.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("flex-container")
+numberOfElements = 20
+
+for (let i = 0; i < numberOfElements; i++) {
+    // Create a new div element
+    const newDiv = document.createElement('div');
+    newDiv.className = "box"
+
+    // Add content to the new div
+    newDiv.innerHTML = `<p style='color:white'>This is restaurant number ${i + 1}</p>`;
+
+    // Append the new div to the container
+    container.appendChild(newDiv);
+}
