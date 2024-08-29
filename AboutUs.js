9d93ac6dad29be91f7df4022a58c068165e59515
Tarek-AL-Saleh <tarek.z.alsaleh@gmail.com>
2024-08-29 19:24:58 +0300

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,12 @@
+footer{
+    position: relative;
+    background-color:#c4edde;
+    padding: 20px;
+    margin-top: 280px;
+    border-top:5px solid #f73859;
+}
+footer p{
+    color:#222e42;
+    font-weight: bold;
+    font-size: 18px;
+}
---
+++ Restaurants.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("flex-container")
+numberOfElements = 24
+
+for (let i = 0; i < numberOfElements; i++) {
+    // Create a new div element
+    const newDiv = document.createElement('div');
+    newDiv.className = "box"
+
+    // Add content to the new div
+    const newImage = document.createElement('img');
+    newImage.src = `restaurant_images/restaurant_image_${i}.png`
+    newImage.style.width = "100%";
+    newImage.style.height = "100%";
+    newImage.style.position = "relative";
+    newImage.style.display = "block";
+    // newDiv.innerHTML = `<p style='color:white'>This is restaurant number ${i + 1}</p>`;
+
+    // Append the new div to the container
+    container.appendChild(newDiv);
+    
+    newDiv.appendChild(newImage);
+    
+}
